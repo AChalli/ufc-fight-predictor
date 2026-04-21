ca9bd6824765f4d1ea2208e4539a7b6793cfb08f
awedc <awedchalli@gmail.com>
2026-04-21 12:38:01 -0400

    Base Model

--- app/main.py
+++ app/main.py
@@ -17,6 +17,6 @@ def root():
 
 @app.get("/fighter/{name}")
 def get_fighter(name: str):
-    df = pd.read_csv("../data/fighters.csv")
+    df = pd.read_csv("../data/ufc_fighters_final.csv")
     fighter = df[df["name"] == name].iloc[0]
     return fighter.to_dict()
